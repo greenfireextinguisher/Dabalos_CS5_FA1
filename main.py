ab9b87ca5a69594b33a2432497419d8829e0274f
greenfireextinguisher <cadabalos@crc.pshs.edu.ph>
2023-08-30 15:26:03 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+friend = "Stewart"
+age = 16
+pronouns = "they/them"
+hobby = "extreme tennis"
+fav_color = "orange"
+fav_fan_setting = 3
+gradeLvl = "grade 11"
+fav_subject = "physics"
+fav_teacher = "sir Chardy"
+fav_game = "google minesweeper"
+
+print("Hey there! I'm", friend)
+user_name = input("What's your name?")
+print("Nice to meet you", user_name,"! How old are you?")
+user_age = int(input())
+print("Cool! I'm",age,".")
+user_hobby = input("Do you have any hobbies?")
+print("That sounds interesting! I play",hobby,".")
+user_favColor = input("So, what's your favorite color?")
+print("I like that color, too! But my personal favorite is",fav_color,". ")
+print("Speaking of the color orange, the sun is burning hot today!")
+user_fanSetting = int(input("What's your favorite setting on the electric fan?"))
+print("Really? Mine is",fav_fan_setting,".")
+print("Anyways, what grade are you in?")
+user_grade = input()
+print("Nice! I'm in",gradeLvl,".")
+user_fav_sub = input("What's your favorite subject?")
+print("Ohh, cool. Mine is", fav_subject,"! :DD")
+user_fav_teacher = input("So, who's your favorite teacher?")
+print("They're great at teaching! My favorite teacher is",fav_teacher,".")
+user_fav_game = input("Anyways, do you play any games?")
+print("That is absolutely fantabulous! I grind on",fav_game,"24/7 bro.")
+print('''Oh my, look at the time! I've gotta go.
+it was really nice meeting you''',user_name,"! I'll see you around :))")
+
+
+
